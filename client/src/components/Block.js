59af522d5057c5a7cb1e6f06eb799aa7195667dd
Sajan Toor <sajantoor98@gmail.com>
2023-07-22 10:56:43 -0700

    Fix bug where you could recapture a block

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 const defaultColor = "gray";
 const nextColor = "blue";
-const timeToSet = 1000; // takes 1 second to change color 
+const timeToSet = 800; // takes 1 second to change color 
 
 export default function Block(props) {
     const [color, setColor] = useState(defaultColor);
@@ -15,7 +15,10 @@ export default function Block(props) {
 
 
     function handleMouseDown(event) {
-        if (event.button !== 0) return; // only left click
+        // only left click
+        if (event.button !== 0) return;
+        // check if block is already "captured"
+        if (color !== defaultColor) return;
 
         // set lastTime to current time 
         setLastTime(Date.now());
