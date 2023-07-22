ba39f420abe21b2be96a56986a6f2626538ff030
Sajan Toor <sajantoor98@gmail.com>
2023-07-22 10:54:22 -0700

    Add transitioning block effect

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,18 +1,49 @@
 import { useEffect, useState } from "react";
 
 
+const defaultColor = "gray";
+const nextColor = "blue";
+const timeToSet = 1000; // takes 1 second to change color 
+
 export default function Block(props) {
-    const [color, setColor] = useState("gray");
+    const [color, setColor] = useState(defaultColor);
+    const [lastTime, setLastTime] = useState(0);
+    const [transition, setTransition] = useState(false);
 
     useEffect(() => {
     }, []);
 
-    function handleClick() {
-        setColor(color === "gray" ? "blue" : "gray");
+
+    function handleMouseDown(event) {
+        if (event.button !== 0) return; // only left click
+
+        // set lastTime to current time 
+        setLastTime(Date.now());
+        setTransition(true);
+        setColor(nextColor);
+    }
+
+    function handleMouseUp(event) {
+        const currentTime = Date.now();
+        const timeElapsed = currentTime - lastTime;
+        console.log(timeElapsed);
+
+
+        if (timeElapsed < timeToSet) {
+            setColor(defaultColor);
+        }
+
+        setTransition(false);
     }
 
     return (
-        <div className="block" onClick={handleClick} style={{ backgroundColor: color }}>
+        <div
+            className={transition ? "block transition" : "block"}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+            style={{ backgroundColor: color }}
+        >
+
         </div>
     );
 }
