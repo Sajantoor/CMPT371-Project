d39014677f68202a3d07b9aa124932333a83bdf4
ashokar19 <arjun.shokar@gmail.com>
2023-07-31 19:08:04 -0700

    added some fault tolerance for server, javadoc

---
+++ backend/ClientHandler.java
@@ -0,0 +1,101 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+/**
+ * Client threads that run while game server hosts.
+ */
+class ClientHandler implements Runnable {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // Handle client communication and game actions here.
+
+            // You can use InputStream/OutputStream to handle communication with the client.
+
+            OutputStream os = clientSocket.getOutputStream();
+            InputStream is = clientSocket.getInputStream();
+            out = new PrintWriter(os, true);
+            in = new BufferedReader(new InputStreamReader(is));
+
+            // Send playerID to the client when connecting
+            String sendPlayerID = "playerID " + Server.getPlayerCount();
+            out.println(sendPlayerID);
+
+            String message;
+
+            while (Server.isPlayersLeft()) {
+                message = in.readLine();
+                handleMessage(message);
+            }
+
+            // Remove the client socket from the list of active client sockets upon
+            // disconnection
+            Server.removeClientSocket(clientSocket);
+            clientSocket.close();
+        } catch (IOException e) {
+            System.err.println("Error handling client: " + e.getMessage());
+        }
+    }
+
+    public void handleMessage(String message) {
+        if (message == null) {
+            return;
+        }
+
+        String[] tokens = message.split(" ");
+
+        if (tokens.length == 0) {
+            return;
+        }
+
+        String commandToken = tokens[0];
+
+        System.out.println(commandToken);
+        broadcastMessage(message);
+
+        switch (commandToken) {
+            case ("cursor"):
+
+                break;
+            case ("draw"):
+
+                break;
+            case ("end"):
+
+                break;
+            case ("capture"):
+
+            case ("playerID"):
+
+            default:
+                System.out.println("Unrecognized command: " + commandToken);
+                break;
+        }
+
+    }
+
+    private void broadcastMessage(String message) {
+        for (Socket socket : Server.getClientSockets()) {
+            if (socket != clientSocket && socket.isConnected()) {
+                try {
+                    PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                    writer.println(message);
+                } catch (IOException e) {
+                    System.err.println("Error broadcasting message to client: " + e.getMessage());
+                }
+            }
+        }
+    }
+}
--- backend/Server.java
+++ backend/Server.java
@@ -1,17 +1,28 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * TCP Server used to accept connections when hosting a game. .
  */
 
 public class Server {
-    private static final int PORT = 8080;
+    private static final int PORT = 3000;
     private static final int MAX_PLAYERS = 4;
-    private static final int BUFFER_SIZE = 1024;
     private static ServerSocket serverSocket = null;
     private static int playerCount = 0;
-    public static void main(String[] args) {
+    private static List<ClientHandler> connectedClients = new ArrayList<>();
+    private static List<Socket> clientSockets = new ArrayList<>();
+    private static boolean isRunning;
+    public static void main(String[] args) throws Exception  {
+        startServer();
+        startFaultTolerance();
+    }
+
+    public static void startServer() throws Exception  {
         serverSocket = null;
         try {
             // Initialize the game board and players (same as previous code)
@@ -21,18 +32,18 @@ public class Server {
             System.out.println("Server listening on port " + PORT);
 
             // Accept connections from clients and handle them
-            playerCount = 0;
             while (playerCount < MAX_PLAYERS) {
                 Socket newSocket = serverSocket.accept();
+                Server.addClientSocket(newSocket);
 
                 // threads for the server to handle multiple clients simultaneously.
                 new Thread(new ClientHandler(newSocket)).start();
-
-                playerCount++;
             }
+            isRunning = true;
         } catch (IOException e) {
             if (playerCount > MAX_PLAYERS) {
-                throw new Exception("The number of target connections cannot exceed  " + MAX_PLAYERS);
+                throw new RuntimeException("The number of target connections cannot exceed  " + MAX_PLAYERS);
+            }
         } finally {
             // Close the server socket
             if (serverSocket != null) {
@@ -44,11 +55,53 @@ public class Server {
             }
         }
     }
-    
-    public void clear() throws IOException {
+
+    public static void stopServer() throws IOException {
+        isRunning = false;
+        clear();
+    }
+
+    public static void clear() throws IOException {
         if (!serverSocket.isClosed()) {
             serverSocket.close();
         }
-        //terminate connections
+        // terminate connections
+    }
+
+    private static void startFaultTolerance() {
+        // Start the heartbeat timer
+        Timer clientTimer = new Timer();
+        clientTimer.schedule(new TimeClients(), 0, 5000); // Every 5 seconds
+    }
+
+    private static class TimeClients extends TimerTask {
+        @Override
+        public void run() {
+            for (ClientHandler clientHandler : connectedClients) {
+                // wasn't sure how we're gonna go about this
+            }
+        }
+    }
+
+    public synchronized static void addClientSocket(Socket socket) {
+        clientSockets.add(socket);
+        playerCount++;
+    }
+
+    public synchronized static void removeClientSocket(Socket socket) {
+        clientSockets.remove(socket);
+        playerCount--;
+    }
+
+    public static List<Socket> getClientSockets() {
+        return clientSockets;
+    }
+
+    public static boolean isPlayersLeft() {
+        return playerCount != 0;
+    }
+
+    public synchronized static int getPlayerCount() {
+        return playerCount;
     }
 }
